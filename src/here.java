3eee977d1ba6f5df4d412ccd7f5e86ac557dd94d
ayushi-006 <ayushidixit716@gmail.com>
2024-10-03 12:29:36 +0530

    commit

--- src/here.java
+++ src/here.java
@@ -1,5 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class here {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        int i = 1;
+        int result;
+        while ( i <= 10){
+            result = N*i;
+            System.out.println(N + " " + "X"+ " "  + i + " " + "=" + " " + result);
+            i++;
+        }
+
 
     }
 }
