deafdf2738240537b68cb63416c3bae11d181727
ayushi-006 <ayushidixit716@gmail.com>
2024-10-04 14:35:33 +0530

    commit

---
+++ src/VarArgs.java
@@ -0,0 +1,41 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+////        fun(2,4,54,67,33,4,2,1);                    //input, any number of arguments
+//      multiple(2,3,"AKEMI","LILY","AUTUMN");
+//    }
+//    static void multiple(int a, int b, String ...v){
+//     static void fun(int ...v){             //store it in an array
+
+
+        // FUNCTION-OVERLOADIING
+
+
+////        fun(43);
+//        fun("akemi");
+//    }
+//
+//        static void fun(int a){
+//            System.out.println("first ans");
+//            System.out.println(a);
+//        }
+//        static void fun(String name){
+//            System.out.println("second ans");
+//            System.out.println(name);
+//
+//        }
+
+
+        demo("lily");
+    }
+        static void demo(int ...a){
+            System.out.println(Arrays.toString(a));
+        }
+        static void demo(String ...a){
+            System.out.println(Arrays.toString(a));
+        }
+    }
+
--- src/swapping.java
+++ src/swapping.java
@@ -1,11 +1,22 @@
 public class swapping {
     public static void main(String[] args) {
-        String name = "Akemi";
-        name = hii(name);                             // Reassign 'name' to the value returned by 'hii'
-        System.out.println(name );            // Now it will print "Meethi"
-    }
-    static String hii( String name) {
-        name = "lily";                      // Change the local 'name' variable
-        return name;                       // Return the new value
+//        String name = "Akemi";
+//        hii(name);
+//        System.out.println(name);
+//    }
+//    static void hii(String nam) {
+//    nam = "lily";
+
+
+                String name = "Akemi";
+                name = hii(name);                      // Reassign 'name' to the value returned by 'hii', this is the diff
+                System.out.println(name );            // Now it will print "Meethi"
+            }
+            static String hii( String name) {
+                name = "lily";                      // Change the local 'name' variable
+                return name;                           // Return the new value
+
+
+
     }
 }
