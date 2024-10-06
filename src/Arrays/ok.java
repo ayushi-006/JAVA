c6b52a903ef04252cb3bb6f395b4ca24a0e287db
ayushi-006 <ayushidixit716@gmail.com>
2024-10-06 10:22:27 +0530

    commit

---
+++ src/Arrays/ok.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class ok {
+    public static void main(String[] args) {
+
+//        String[] arr = new String[4];
+//        System.out.println(arr[0]);
+        Scanner in = new Scanner(System.in);
+
+        int[] arr = new int[5];
+         for (int i = 0 ; i < arr.length ; i++){
+             arr[i] = in.nextInt();
+             System.out.print(arr[i] + " ");
+         }
+
+
+    }
+
+
+
+
+
+
+}
